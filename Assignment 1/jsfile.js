b58ce040063d7013cdcf16fd1b3f2e253d8ff0fc
Pandey <utkarshpandey8@deloitte.com>
2023-05-05 18:52:19 +0530

    day 1 assignment

--- Assignment 1/jsfile.js
+++ Assignment 1/jsfile.js
@@ -5,3 +5,9 @@ let myCity = "Patna"
 document.write("Hello, my name is "+myName+"<br/>")
 document.write("Hello, my name in italics is <i>"+ myName+"</i><br/>")
 document.write("I was born in the city of "+myCity)
+
+let myHobbies = ["Watching sports", "Playing video games","Exploring new places","Listening to rock/metal"]
+
+document.write("<h3>My hobbies are :</h3>")
+document.write("<ul><li>"+myHobbies[0]+"</li><li>"+myHobbies[1]+"</li>")
+document.write("<li>"+myHobbies[2]+"</li><li>"+myHobbies[3]+"</li></ul>")
